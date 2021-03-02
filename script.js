1c9ba476013c116ea3261b788d09e6722a409532
mikamimochi <78176268+mikamimochi@users.noreply.github.com>
2021-03-02 15:00:21 -0800

    Project 2

---
+++ script.js
@@ -0,0 +1,127 @@
+var counter = 0;
+
+function togglePopup(){
+  document.getElementById("box_id").classList.toggle("active");
+  $("#but").hide();
+  counter++;
+  if(counter == 28){
+  	location.reload();
+  }
+}
+
+function togglePopup2(){
+  document.getElementById("box_id2").classList.toggle("active2");
+  $("#but2").hide();
+  counter++;
+  if(counter == 28){
+  	location.reload();
+  }
+}
+
+function togglePopup4(){
+  document.getElementById("box_id4").classList.toggle("active4");
+  $("#but4").hide();
+  counter++;
+  if(counter == 28){
+    location.reload();
+  }
+}
+
+function togglePopup5(){
+  document.getElementById("box_id5").classList.toggle("active5");
+  $("#but5").hide();
+  counter++;
+  if(counter == 28){
+    location.reload();
+  }
+}
+
+function togglePopup6(){
+  document.getElementById("box_id6").classList.toggle("active6");
+  $("#but6").hide();
+  counter++;
+  if(counter == 28){
+    location.reload();
+  }
+}
+
+function togglePopup7(){
+  document.getElementById("box_id7").classList.toggle("active7");
+  $("#but7").hide();
+  counter++;
+  if(counter == 28){
+    location.reload();
+  }
+}
+
+function togglePopup8(){
+  document.getElementById("box_id8").classList.toggle("active8");
+  $("#but8").hide();
+  counter++;
+  if(counter == 28){
+    location.reload();
+  }
+}
+
+function togglePopup9(){
+  document.getElementById("box_id9").classList.toggle("active9");
+  $("#but9").hide();
+  counter++;
+  if(counter == 28){
+    location.reload();
+  }
+}
+
+function togglePopup10(){
+  document.getElementById("box_id10").classList.toggle("active10");
+  $("#but10").hide();
+  counter++;
+  if(counter == 28){
+    location.reload();
+  }
+}
+
+function togglePopup11(){
+  document.getElementById("box_id11").classList.toggle("active11");
+  $("#but11").hide();
+  counter++;
+  if(counter == 28){
+    location.reload();
+  }
+}
+
+function togglePopup12(){
+  document.getElementById("box_id12").classList.toggle("active12");
+  $("#but12").hide();
+  counter++;
+  if(counter == 28){
+    location.reload();
+  }
+}
+
+function togglePopup13(){
+  document.getElementById("box_id13").classList.toggle("active13");
+  $("#but13").hide();
+  counter++;
+  if(counter == 28){
+    location.reload();
+  }
+}
+
+function togglePopup14(){
+  document.getElementById("box_id14").classList.toggle("active14");
+  $("#but14").hide();
+  counter++;
+  if(counter == 28){
+    location.reload();
+  }
+}
+
+function togglePopup15(){
+  document.getElementById("box_id15").classList.toggle("active15");
+  $("#but15").hide();
+  counter++;
+  if(counter == 28){
+    location.reload();
+  }
+}
